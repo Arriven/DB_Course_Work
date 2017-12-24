1a601acabe0f4e0e1163fd55e60b9943be3be27f
Arriven <bogdannuch@gmail.com>
2017-12-24 16:13:17 +0200

    move dbsetup to TestMain

--- main_test.go
+++ main_test.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"testing"
+	"os"
+	"os/exec"
 )
 
-func TestMain(t *testing.T) {
+func TestMain(m *testing.M){
+	cmd := exec.Command("psql", "-f dbsetup.sql -U postgres")
+	cmd.Run()
+	os.Exit(m.Run())
+}
+
+func TestNormalFlow(t *testing.T) {
 	err := PrintDb("postgres", "", "course_db")
 	if(err != nil) {
 		t.Error(err)
